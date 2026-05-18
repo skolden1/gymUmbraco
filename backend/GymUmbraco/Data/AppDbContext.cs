f90126bd56376c6c26010acf9102325add776aec
skolden1 <skolden123@hotmail.com>
2026-05-18 11:52:24 +0200

    updated DB w more exercises through seeding

--- backend/GymUmbraco/Data/AppDbContext.cs
+++ backend/GymUmbraco/Data/AppDbContext.cs
@@ -14,5 +14,45 @@ namespace GymUmbraco.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Workout> Workouts { get; set; }
         public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
-}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise { Id = 1, ExerciseName = "Bänkpress" },
+                new Exercise { Id = 2, ExerciseName = "Lutande hantelpress" },
+                new Exercise { Id = 3, ExerciseName = "Kabel-flyes" },
+                new Exercise { Id = 4, ExerciseName = "Dips" },
+
+                new Exercise { Id = 5, ExerciseName = "Pullups" },
+                new Exercise { Id = 6, ExerciseName = "Pullups smalt grepp" },
+                new Exercise { Id = 7, ExerciseName = "Latsdrag" },
+                new Exercise { Id = 8, ExerciseName = "Skivstångsrodd" },
+                new Exercise { Id = 9, ExerciseName = "Hantelrodd" },
+                new Exercise { Id = 10, ExerciseName = "Sittande kabelrodd" },
+                new Exercise { Id = 11, ExerciseName = "Shrugs" },
+
+                new Exercise { Id = 12, ExerciseName = "Militärpress" },
+                new Exercise { Id = 13, ExerciseName = "Sittande hantelpress" },
+                new Exercise { Id = 14, ExerciseName = "Sidolyft" },
+                new Exercise { Id = 15, ExerciseName = "Bakre axlar maskin" },
+                new Exercise { Id = 16, ExerciseName = "Face Pulls" },
+
+                new Exercise { Id = 17, ExerciseName = "Knäböj" },
+                new Exercise { Id = 18, ExerciseName = "Marklyft" },
+                new Exercise { Id = 19, ExerciseName = "Benpress" },
+                new Exercise { Id = 20, ExerciseName = "Benspark" },
+                new Exercise { Id = 21, ExerciseName = "Lårcurl" },
+                new Exercise { Id = 22, ExerciseName = "Vadpress" },
+                new Exercise { Id = 23, ExerciseName = "Bulgariska utfall" },
+
+                new Exercise { Id = 24, ExerciseName = "Bicepscurl" },
+                new Exercise { Id = 25, ExerciseName = "Hammercurl" },
+                new Exercise { Id = 26, ExerciseName = "Triceps pushdown" },
+                new Exercise { Id = 27, ExerciseName = "Tricepspress med hantel" }
+            );
+        }
+    }
 }
---
+++ backend/GymUmbraco/Migrations/20260518092545_SeedExercises.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GymUmbraco.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedExercises : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Exercises",
+                columns: new[] { "Id", "ExerciseName", "PictureUrl" },
+                values: new object[,]
+                {
+                    { 1, "Bänkpress", null },
+                    { 2, "Lutande hantelpress", null },
+                    { 3, "Kabel-flyes", null },
+                    { 4, "Dips", null },
+                    { 5, "Pullups", null },
+                    { 6, "Pullups smalt grepp", null },
+                    { 7, "Latsdrag", null },
+                    { 8, "Skivstångsrodd", null },
+                    { 9, "Hantelrodd", null },
+                    { 10, "Sittande kabelrodd", null },
+                    { 11, "Shrugs", null },
+                    { 12, "Militärpress", null },
+                    { 13, "Sittande hantelpress", null },
+                    { 14, "Sidolyft", null },
+                    { 15, "Bakre axlar maskin", null },
+                    { 16, "Face Pulls", null },
+                    { 17, "Knäböj", null },
+                    { 18, "Marklyft", null },
+                    { 19, "Benpress", null },
+                    { 20, "Benspark", null },
+                    { 21, "Lårcurl", null },
+                    { 22, "Vadpress", null },
+                    { 23, "Bulgariska utfall", null },
+                    { 24, "Bicepscurl", null },
+                    { 25, "Hammercurl", null },
+                    { 26, "Triceps pushdown", null },
+                    { 27, "Tricepspress med hantel", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 27);
+        }
+    }
+}
