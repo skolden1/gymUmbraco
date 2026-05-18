80fa03c03f723b7c67caf9c6812bed436ec94695
skolden1 <skolden123@hotmail.com>
2026-05-18 09:41:29 +0200

    fixed hamburger navbar, states, styling done for now

--- frontend/gym-frontend/src/components/Navbar.tsx
+++ frontend/gym-frontend/src/components/Navbar.tsx
@@ -1,31 +1,42 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { FiMenu, FiX  } from "react-icons/fi";
 import "./Navbar.css";
+import { useState } from "react";
 
 
 const Navbar = () => {
 
   const {logout, user} = useAuth(); 
 
+  const [toggle, setToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setToggle(prev => !prev)
+    console.log("toggled")
+  }
+
   return (
     <div>
       <header>
         <nav>
-          <ul>
-            <div className="navLeft">
-              <li>
-                <Link to="/dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/createProgram">Skapa program</Link>
-              </li>
-            </div>
+          
+          {toggle ? <FiX onClick={toggleMenu} className="burgerCrossIcon"/> : <FiMenu onClick={toggleMenu} className="burgerIcon" />}
+            <ul className={toggle ? "activeMenu" : ""}>
+              <div className="navLeft">
+                <li>
+                  <Link to="/dashboard">Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/createProgram">Skapa program</Link>
+                </li>
+              </div>
 
-            <li className="row">
-              Välkommen {user?.email}
-              <button className="logoutBtn" onClick={logout}>Logga ut</button>
-            </li>
-          </ul>
+              <li className="row">
+                Välkommen {user?.email}
+                <button className="logoutBtn" onClick={logout}>Logga ut</button>
+              </li>
+            </ul> 
         </nav>
       </header>
     </div>
